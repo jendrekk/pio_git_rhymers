38dbf3f0dd9c8d1814b487c380578d0e2f768084
Jędrzej Pokrywiecki <250992@edu.p.lodz.pl>
2026-03-18 17:19:16 +0100

    3. Scalenie galezi format z uzyciem squash

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,14 +2,23 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
+<<<<<<< HEAD
 	int totalRejected = 0;
+=======
+    int totalRejected = 0;
+>>>>>>> origin/format
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+<<<<<<< HEAD
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
+=======
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+>>>>>>> origin/format
             totalRejected++;
         else
             super.countIn(in);
